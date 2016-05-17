01ae4e6d56b43e0726a870c0b43252f50edfd481
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air-2.local>
2016-05-17 16:39:55 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427021125) do
+ActiveRecord::Schema.define(version: 20160517032138) do
 
   create_table "trains", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "title",      limit: 255
     t.text     "text",       limit: 65535
     t.text     "image",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "link1",      limit: 65535
+    t.text     "link2",      limit: 65535
+    t.text     "link3",      limit: 65535
   end
 
 end
