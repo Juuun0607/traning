b213834831c78020771851acc6aa28f823d01075
tcampryouzi <ryouzi5136saitou@fuji.waseda.jp>
2016-05-17 12:32:01 +0900

    create actions

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -5,11 +5,23 @@ class TrainsController < ApplicationController
   end
 
   def show
-    
+  end
+
+  def new
   end
 
   def create
   end
-  
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'trains' => 'trains#index'
+  root 'trains#index'
   get 'trains/:id' => 'trains#show'
 end
