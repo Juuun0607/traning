8b16d763ae9c039abbed8dca8dd25835695a1222
tcampryouzi <ryouzi5136saitou@fuji.waseda.jp>
2016-05-17 13:24:00 +0900

    create devise layout file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:username)
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
