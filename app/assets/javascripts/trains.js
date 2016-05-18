8fc37a1979c0c82e9fca2dd203da9ed32a5dc25e
斉藤良二 <saitouryouji@saitouryouji-no-MacBook-Air.local>
2016-05-18 22:27:43 +0900

    fix button

--- app/assets/javascripts/trains.js
+++ app/assets/javascripts/trains.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-   $('.drawer').drawer();
+$(function() {
+  var $header = $('#top-head');
+  // Nav Fixed
+  $(window).scroll(function() {
+      if ($(window).scrollTop() > 350) {
+          $header.addClass('fixed');
+      } else {
+          $header.removeClass('fixed');
+      }
+  });
+  // Nav Toggle Button
+  $('#nav-toggle').click(function(){
+      $header.toggleClass('open');
+  });
 });
