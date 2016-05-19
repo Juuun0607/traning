3431cf655a91f102f808a6eeb63fc7c20f941d40
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air-2.local>
2016-05-19 11:55:17 +0900

    no message

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -2,7 +2,7 @@ class TrainsController < RankingController
 
   def index
     @q = Train.search(params[:q])
-    @trains = @q.result(distinct: true).page(params[:page]).per(5)
+    @trains = @q.result(distinct: true).page(params[:page]).per(5).order("id DESC")
   end
 
   def show
